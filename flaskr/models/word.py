5d7005ee8d0744f4f16254e05feb054797e8df32
Błażej <darullef4@gmail.com>
2019-11-15 23:15:25 +0100

    Utworzenie tabeli pivot dla klas word i file.

--- flaskr/models/word.py
+++ flaskr/models/word.py
@@ -1,5 +1,6 @@
 from datetime import datetime as dt
 from . import db
+from .pivot_word_file import wordFile
 
 
 class Word(db.Model):
@@ -8,6 +9,8 @@ class Word(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
     word = db.Column(db.VARCHAR, nullable=False)
     created = db.Column(db.TIMESTAMP, nullable=False)
+    file = db.relationship('File', secondary=wordFile, lazy='subquery',
+                           backref=db.backref('words', lazy=True))
 
     def __repr__(self):
         return '<Word {}>'.format(self.word)
