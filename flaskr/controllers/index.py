552a3a53e3b96b0ec8fd7e9e414415ef167944b9
Błażej <darullef4@gmail.com>
2019-11-21 01:32:48 +0100

    Utworzenie funkcji dodająca wpisy do bazy danych z pliku użytkownika.

--- flaskr/controllers/index.py
+++ flaskr/controllers/index.py
@@ -1,15 +1,13 @@
 from flask import Blueprint
-from flaskr.models.word import Word, createWord
-from flaskr.models.file import File, createFile
+from flaskr.services.read_file import readFileAndCreateDB
 
 bp = Blueprint('index', __name__)
 
 
 @bp.route('/')
 def index():
-    f = createFile("first")
-    w = createWord("tree", f.id, 20)
-    return "Hello world!" + str(w)
+    readFileAndCreateDB()
+    return "Hello world!"
 
 
 @bp.route('/<string:name>')
