1e30e976707362406dc407bea69e1fa88019adc4
baatheo <44375281+baatheo@users.noreply.github.com>
2019-11-28 21:55:54 +0100

    Update index.py

--- flaskr/controllers/index.py
+++ flaskr/controllers/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, json, jsonify
+from flask import Blueprint, render_template, request, jsonify
 from flaskr.services.read_file import readFileAndCreateDB
 from flaskr.services.spell_check import checkWord
 
