658705cbf7c975e02b60ca572ff4c4a933722b0e
Bartek Olszanowski <baloo@int.pl>
2020-02-29 23:17:40 +0100

    Kompatybilność z serwerem Phusion Passenger

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,32 +3,27 @@ import os
 from flask import Flask
 from flaskr.models import db
 
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_aspell.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-def create_app():
+db.init_app(app)
 
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_aspell.db'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    db.init_app(app)
+with app.app_context():
+    from . import models, controllers, services
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    with app.app_context():
-
-        from . import models, controllers, services
-        db.create_all()
-        models.init_app(app)
-        controllers.init_app(app)
-        services.init_app(app)
-
-        return app
+    db.create_all()
+    models.init_app(app)
+    controllers.init_app(app)
+    services.init_app(app)
