7b0bcced0b5c1698dc08deb4a08c0597b1b3c0a1
Bartek Olszanowski <baloo@int.pl>
2020-02-29 23:57:12 +0100

    Usunięcie niepotrzebnych endpointów; Poprawienie wyglądu

--- front/js/components/Footer.vue
+++ front/js/components/Footer.vue
@@ -1,12 +1,15 @@
 <template>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        Błażej Darul, Jakub Florczak, <a href="https://github.com/baloo1379">Bartłomiej Olszanowski</a>, Bartosz Porębski. The source code and website content is licensed under
-        <a href="https://github.com/baloo1379/db_aspell/blob/0.2/LICENSE">MIT</a>.
-      </p>
-    </div>
-  </footer>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <a href="https://github.com/darullef">Błażej Darul</a>,
+                <a href="https://github.com/flopczak">Jakub Florczak</a>,
+                <a href="https://github.com/baloo1379">Bartłomiej Olszanowski</a>,
+                <a href="https://github.com/baatheo">Bartosz Porębski</a>. The source code and website content is licensed under
+                <a href="https://github.com/baloo1379/db_aspell/blob/master/LICENSE">MIT</a>.
+            </p>
+        </div>
+    </footer>
 </template>
 
 <script>
